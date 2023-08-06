97c10047bb294d4f8f4ba60e9adebd9c35ac287f
keithdavid24 <keithdavid247@gmail.com>
2023-08-05 18:31:00 -0600

    U2_01 Partial

--- script.js
+++ script.js
@@ -6,17 +6,34 @@
 function randomNumber(userGuess, computersNumber) {
 
     // YOUR CODE BELOW
+    let smallValue = 1;
+    let largeValue = 100;
 
+    if (userGuess < smallValue || userGuess > largeValue) {
 
+        return "Guess needs to be between 1-100";
+    }
+
+    if (userGuess == computersNumber) {
+        return (`The number was ${userGuess}`);
+    } else if (userGuess < computersNumber) {
+        return ("Higher");
+    } else {
+        return ("Lower");
+    }
 
 
     // YOUR CODE ABOVE
 };
+// Global Variable
+let currentNumber = 50;
+let lowest = 1;
+let highest = 100;
 
 
 
 //* Have the Computer Guess your Number ---------------------------------------------
-/* 
+/*
     Build out the functionality for the computer to guess and evaluate your random number.
 
     - There are two static functions framed. DO NOT alter the name or parameters of these functions:
@@ -34,16 +51,56 @@ function startCompGuess(num) {
     // This should return a string that denotes the first guessed number
 
     // YOUR CODE ...
+    return (`Is your number ${currentNumber}`);
+}
+// Block Arrow Function
+let createGuess = () => {
+    currentNumber = Math.floor((Math.random() * 100) + 1);
+    return currentNumber;
+}
 
+function onLowerButtonClick() {
+    highest = currentNumber - 1;
+    currentNumber = createGuess();
+    return `Your number is lower? is it the ${currentNumber}`;
+}
+function onHigherButtonClick() {
+    lowest = currentNumber + 1;
+    currentNumber = createGuess();
+    return `Your number is higher? Is it ${currentNumber}?`;
+}
 
+function ohThatsItButtonClick() {
+    return `I knew it was ${currentNumber}`;
 }
 
 function compGuess(reply) {
-    /* 
+    /*
     *   The parameter "reply" will either be passing "lower", "correct", or "higher". This should be considered when evaluating th elogic and response.
 
     This should return a string indicating the computers response.
     */
 
+    switch (reply) {
+        case "lower":
+            highest = currentNumber - 1;
+            currentNumber = Math.floor((lowest + highest) / 2);
+            return `Your number is lower? Is it ${currentNumber}?`;
+        case "higher":
+            lowest = currentNumber + 1;
+            currentNumber = Math.floor((lowest + highest) / 2);
+            return `Your number is higher? Is it ${currentNumber}?`;
+        case "correct":
+            return `I knew it was ${currentNumber}!`;
+        default:
+            return "Invalid response. Please reply with 'lower', 'correct', or 'higher'.";
+    }
 }
+
+// Test the startCompGuess function after reassigning currentNumber.
+console.log(startCompGuess());
+
+// console.log(compGuess('lower'));
+// console.log(compGuess('higher'));
+// console.log(compGuess('correct'));
 
